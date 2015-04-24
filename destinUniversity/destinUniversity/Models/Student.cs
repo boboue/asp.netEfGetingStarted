5329216ccf78ebedc83e0ecb6588d90fb643dfd9
boboue <bobouechancelve@gmail.com>
2015-04-24 16:13:28 -0700

    installation entity Framework;
    added student model ;

---
+++ destinUniversity/destinUniversity/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace destinUniversity.Models
+{
+    public class Student
+    {
+
+        public int ID { get; set; }
+
+        public string LastName { get; set; }
+        public string FristMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollment { get; set; }
+    }
+}
